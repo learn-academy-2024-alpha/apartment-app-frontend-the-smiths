0102ceeb6af88acc6e471f683b4969d652c1bc87
mark19242 <mark19242@gmail.com>
2024-04-11 16:53:49 -0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react"
+import { Routes, Route } from "react-router-dom"
 import mockUsers from "./mockUsers.js"
 import mockApartments from "./mockApartments.js"
 import "./App.css"
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
+import Home from "./pages/Home.js"
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState(mockUsers[0])
@@ -12,6 +14,9 @@ const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </>
   )
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div className="card-container">
+      <div className="card-left"></div>
+      <div className="card-right"></div>
+    </div>
+  )
+}
+
+export default Home
