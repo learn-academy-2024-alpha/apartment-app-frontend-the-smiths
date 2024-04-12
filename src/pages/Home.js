0b6879b2620e3d7e785060fc287ccd35f530907d
mark19242 <mark19242@gmail.com>
2024-04-12 10:32:49 -0700

    home page styled and tested

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,18 @@ import React from "react"
 
 const Home = () => {
   return (
-    <div className="card-container">
-      <div className="card-left"></div>
-      <div className="card-right"></div>
-    </div>
+    <>
+      <div className="card-container" data-testid="card-container">
+        <div className="card-left"></div>
+      </div>
+      <div className="hero-text">
+        <h2 className="hero-h2">Live the Vibe, Love Your Life</h2>
+        <h3>
+          As the undisputed leader in luxury apartment rentals, we offer an
+          unparalleled experience that redefines elegance and comfort.
+        </h3>
+      </div>
+    </>
   )
 }
 
