540758c7e2350385e773357a23f6416d3b067d33
mark19242 <mark19242@gmail.com>
2024-04-18 14:11:38 -0700

    added sound

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
+import Sound from "react-sound"
 
 const Footer = () => {
+  const [isPlaying, setIsPlaying] = useState(false)
+  const audioSrc = "/gentlewaves.mp3"
+
+  const togglePlayPause = () => {
+    const audio = document.getElementById("audio-player")
+    if (isPlaying) {
+      audio.pause()
+    } else {
+      audio.play()
+    }
+    setIsPlaying(!isPlaying)
+  }
   return (
     <footer className="footer-text">
+      <div className="audio-player-container">
+        <audio
+          id="audio-player"
+          src={audioSrc}
+          loop // This makes the audio loop
+          onPlay={() => setIsPlaying(true)}
+          onPause={() => setIsPlaying(false)}
+        >
+          Your browser does not support the audio element.
+        </audio>
+        <button onClick={togglePlayPause}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
+
       <p style={{ color: "#e7b47f" }}>
         © 2024{" "}
         <a
